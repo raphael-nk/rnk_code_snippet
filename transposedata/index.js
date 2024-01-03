c2fdf5ebf1913bb7384defe866baab7baece3e28
Raphaël <raphael.nk.dev@gmail.com>
2024-01-03 12:28:47 +0300

    refact:update data in transpose_js

--- transposedata/index.js
+++ transposedata/index.js
@@ -1,20 +1,20 @@
 const data = [
   {
-    "_id": "65643d9d8ac85e0d8f034842",
-    "user_id": "652d4c3e44225f3fe6012b03",
-    "exchange_id": "615aaae93f4ccf38515f9b7b",
+    "_id": "65643d9d8ac85e",
+    "user_id": "652d4c3e44225f",
+    "exchange_id": "615aaae93f4c",
     "is_active": true,
     "usdt_limit": 2000,
     "usdt_value": 2000,
     "usdt_plus_value": 10,
-    "btc_amount": 0.00023698999999999999254741978038651950555504299700260162353515625,
+    "btc_amount": 0.00023698,
     "updated_at": "2023-12-11T08:01:15.431000Z",
     "exchange": "BINANCE"
   },
   {
-    "_id": "6573210e3f2a009279023002",
-    "user_id": "652d4c3e44225f3fe6012b03",
-    "exchange_id": "642692fd8b623bac46d8819e",
+    "_id": "6573210e3f2a00",
+    "user_id": "652d4c3e4422",
+    "exchange_id": "642692fd8b623",
     "is_active": true,
     "usdt_limit": 200,
     "usdt_value": 200,
